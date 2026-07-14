6ce85156efc6a98555d2b8748be384fdbac83d88
Your Name <your@email.com>
2026-07-14 22:31:07 +0530

    Add global Material UI theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme/theme";
+
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+
+    </ThemeProvider>
   </React.StrictMode>
 );
